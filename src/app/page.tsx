84f13134f9b460ea0228bc50bd10e77cee0591b4
Neil Andre Ibona <neilandreibona@gmail.com>
2026-01-15 15:05:53 +0800

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/dashboard/Hero";
+import SectionTab from "@/components/dashboard/Section";
 
 export default function Home (){
     return(
         <main className="min-h-screen bg-white">  
           <Hero />  
+          <SectionTab />
         </main>
     );
 
